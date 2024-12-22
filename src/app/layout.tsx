5cd9301b646efcad38a89390d942eb66cd1181d0
Anna Valentino <pusheen1308@gmail.com>
2024-12-22 10:33:58 -0600

    change layout?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Urbanist } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
-const urbanist = Urbanist({
+const montserrat = Montserrat({
   subsets: ["latin"],
 });
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-slate-900 text-slate-100">
       <body
-        className={`${urbanist.className}  antialiased`}
+        className={`${montserrat.className}  antialiased`}
       >
         {children}
       </body>
