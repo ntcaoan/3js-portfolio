df19cf6a9e1fb6cb1e33d38973429df4ae26919b
Anna Valentino <pusheen1308@gmail.com>
2024-12-27 00:25:07 -0600

    adding more 3d shapes around

--- src/slices/Hero/Shapes.jsx
+++ src/slices/Hero/Shapes.jsx
@@ -34,10 +34,39 @@ export default function Shapes() {
 
 function Geometries() {
     const geometries = [
+        // center boi
         {
             position: [0, 0, 0],
             r: 0.3,
             geometry: new THREE.IcosahedronGeometry(3), // Gem
+        },
+
+        // pill boi
+        {
+            position: [1, -0.75, 4],
+            r: 0.4,
+            geometry: new THREE.CapsuleGeometry(0.5, 1.6, 2, 16), // Pill
+        },
+
+        // this one look like the center one
+        {
+            position: [-1.4, 2, -4],
+            r: 0.6,
+            geometry: new THREE.DodecahedronGeometry(1.5), // Soccer Ball
+        },
+
+        // donut
+        {
+            position: [-0.8, -0.75, 5],
+            r: 0.5,
+            geometry: new THREE.TorusGeometry(0.6, 0.25, 16, 32), // donut
+        },
+
+        // diamond
+        {
+            position: [1.6, 1.6, -4],
+            r: 0.7,
+            geometry: new THREE.OctahedronGeometry(1.5), // diamond
         },
     ];
 
