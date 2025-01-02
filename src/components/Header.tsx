19d6d3f511d0955ddd997138f29697f18c712dba
Anna Valentino <pusheen1308@gmail.com>
2025-01-02 12:42:14 -0600

    starting with header, setup in slicemachine

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { createClient } from "@/prismicio";
+
+export default async function Header() {
+    const client = createClient();
+    const settings = await client.getSingle("settings");
+
+    return (
+        <header className="">
+
+        </header>
+    );
+}
