7e084b87cc138d9104f809dca178a115f89efb4c
Anna Valentino <pusheen1308@gmail.com>
2025-01-10 09:22:35 -0600

    add another comment period

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -4,5 +4,6 @@ export default function ContentList() {
     return (
         <div>ContentList</div>
     //     is this for the blog and projects thingy??
+    //     i add another comment cuz i confused
     )
 }
